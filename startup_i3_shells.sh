4ad4fa0dbfb605417b1bf05e75cccb2e7847246d
Benedikt Wolters <benedikt.wolters@googlemail.com>
2013-10-10 12:41:50 +0200

    update

--- startup_i3_shells.sh
+++ startup_i3_shells.sh
@@ -1,41 +1,16 @@
-#! /bin/sh
-
-# Wait for program
-wait_for () {
-    echo wait
-    n=0
-    while true
-    do
-    if $1; then
-        echo f
-        break
-    else
-        echo mmm
-        # 10 seconds timeout
-        if [ $n -eq 10000 ]; then
-        xmessage "Error on: $1"
-        break
-        else
-        n=`expr $n + 1`
-        sleep .001
-        fi
-    fi
-    done
-    sleep 1
-}
-
-lxterminal &
-wait_for "xdotool search --onlyvisible --pid $! --name lxterminal"
-i3-msg split h
-lxterminal &
-wait_for "xdotool search --onlyvisible --pid $! --name lxterminal"
-i3-msg resize grow width 20 px or 20 ppt
-i3-msg split v
-lxterminal &
-wait_for "xdotool search --onlyvisible --pid $! --name lxterminal"
-i3-msg focus parent
-i3-msg split h
-lxterminal &
-wait_for "xdotool search --onlyvisible --pid $! --name lxterminal"
-i3-msg 'workspace 4-Spotify'
-spotify&
+#!/bin/bash
+i3-msg 'focus parent; layout splith'
+~/Scripts/i3-exec-wait/i3-exec-wait lxterminal
+~/Scripts/i3-exec-wait/i3-exec-wait lxterminal
+i3-msg 'resize grow width 20 px or 20 ppt'
+i3-msg 'split v'
+~/Scripts/i3-exec-wait/i3-exec-wait lxterminal
+i3-msg 'focus left; split v'
+~/Scripts/i3-exec-wait/i3-exec-wait lxterminal
+i3-msg 'focus up'
+i3-msg 'focus parent'
+i3-msg 'focus right'
+i3-msg 'split h'
+~/Scripts/i3-exec-wait/i3-exec-wait lxterminal
+exec i3-msg 'workspace 4-Spotify;'
+~/Scripts/i3-exec-wait/i3-exec-wait spotify
